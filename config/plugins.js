15956fb702795fc7e8663fe0aca1e54fbb5d1ac7
aimanharith67 <harith.aiman@celcom.com.my>
2022-02-14 22:48:30 +0800

    cloud

--- config/plugins.js
+++ config/plugins.js
@@ -9,6 +9,14 @@ module.exports = ({ env }) => ({
             defaultReplyTo: "aimanharith67@gmail.com"
         },
     },
+    upload: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        },
+    }
 });
 
 // module.exports = ({ env }) => ({
